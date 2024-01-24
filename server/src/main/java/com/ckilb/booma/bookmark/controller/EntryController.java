7ac57ad5565d9bdb48ca283cfd72aa0b9903bc16
Christian Kilb <christian.kilb@gmail.com>
2024-01-24 10:39:35 +0100

    hide referer when clicking on link

--- server/src/main/java/com/ckilb/booma/bookmark/controller/EntryController.java
+++ server/src/main/java/com/ckilb/booma/bookmark/controller/EntryController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.Collection;
 
 @RestController
